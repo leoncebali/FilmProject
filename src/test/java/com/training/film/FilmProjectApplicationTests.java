2f19fe44c5ae7eb6397c8f52ee8f38243dc4a9f1
leoncebali <jaimerajkat@gmail.com>
2020-10-26 22:04:06 -0400

    initial commit

---
+++ src/test/java/com/training/film/FilmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
